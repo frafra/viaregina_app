8853f22eed86a0c625225ac85876d48136a64fe0
Francesco Frassinelli <fraph24@gmail.com>
2017-10-11 22:29:36 +0200

    Protocol is needed by PouchDB

--- www/settings.js
+++ www/settings.js
@@ -1,4 +1,4 @@
-let DATABASE = `//${document.location.hostname}:5984`;
+let DATABASE = `http://${document.location.hostname}:5984`;
 var SETTINGS = {
   db_points_url:`${DATABASE}/db_points_url`,
   db_users_url:`${DATABASE}/db_users_url`
